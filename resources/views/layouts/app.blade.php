af6a3e4a204b1c21a5a2a5ff143b1cddb1588651
Nguyen Duc Dat <datnd189@gmail.com>
2017-09-30 22:10:32 +0900

    fix css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>blog</title>
-	<link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 	<script src="//code.jquery.com/jquery.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
